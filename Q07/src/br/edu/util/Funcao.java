87ee784c79c69049169fc0d32acce9e0394527fa
Gabriel Andrade <gabriel.santos08@aluno.ifce.edu.br>
2024-12-03 23:51:19 -0300

    Update Funcao.java

--- Q07/src/br/edu/util/Funcao.java
+++ Q07/src/br/edu/util/Funcao.java
@@ -1,10 +1,10 @@
 package br.edu.util;
 public class Funcao {
 
-	public static double calcula_media(double Num1, double Num2, double Num3, String l) {
+	public static double calcula_media(double Num1, double Num2, double Num3, char letra) {
         double media;
 
-        if (l.equals("A")) {
+        if (letra == 'A') {
             media = (Num1 + Num2 + Num3) / 3;  
         } else {
             media = (Num1 * 5 + Num2 * 3 + Num3 * 2) / (5 + 3 + 2);  
